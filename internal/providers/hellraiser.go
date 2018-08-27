c0219d9b2233abf0d0d8ccd8c39fee51242369d7
Robbie Trencheny <me@robbiet.us>
2018-08-27 01:40:24 -0700

    Remove providers package

--- internal/providers/hellraiser.go
+++
@@ -1,4 +0,0 @@
-package providers
-
-// Playlist URL: http://liquidit.info:8080/get.php?username=xxxx&password=xxxxxxx&type=m3uplus&output=ts
-// XMLTV URL: http://liquidit.info:8080/xmltv.php?username=xxxxxx&password=xxxxxx
